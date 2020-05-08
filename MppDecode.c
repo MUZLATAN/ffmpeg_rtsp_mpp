d17df62a66ef266dd26a47e1de12bc55011338a8
MUZLATAN <37372290+MUZLATAN@users.noreply.github.com>
2020-05-08 17:36:34 +0800

    添加了yuv数据转opencv Mat的函数， 可以了解yuv内存数据的存储方式

--- MppDecode.c
+++ MppDecode.c
@@ -3,8 +3,60 @@
 //
 
 #include "MppDecode.h"
-
+#incldue "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp
+#include "opencv2/imgcodecs/imgcodecs.hpp"
 int frame_null = 0;
+void MppDecode::conv_yuv420_to_mat()
+{
+    RK_U32 width    = 0;
+    RK_U32 height   = 0;
+    RK_U32 h_stride = 0;
+    RK_U32 v_stride = 0;
+
+    MppBuffer buffer    = NULL;
+    RK_U8 *base = NULL;
+
+    width    = mpp_frame_get_width(frame);
+    height   = mpp_frame_get_height(frame);
+    h_stride = mpp_frame_get_hor_stride(frame);
+    v_stride = mpp_frame_get_ver_stride(frame);
+    buffer   = mpp_frame_get_buffer(frame);
+
+    base = (RK_U8 *)mpp_buffer_get_ptr(buffer);
+    RK_U32 buf_size = mpp_frame_get_buf_size(frame);
+    size_t base_length = mpp_buffer_get_size(buffer);
+    mpp_log("base_length = %d\n",base_length);
+
+    RK_U32 i;
+    RK_U8 *base_y = base;
+    RK_U8 *base_c = base + h_stride * v_stride;
+
+    cv::Mat yuvImg;
+    yuvImg.create(height*3/2, width, CV_8UC1);
+
+    //转为YUV420sp格式
+    int idx = 0;
+
+    for (i = 0; i < height; i++, base_y += h_stride) {
+//        fwrite(base_y, 1, width, fp);
+        memcpy(yuvImg.data+idx, base_y, width);
+        idx += width;
+    }
+    for (i = 0; i < height / 2; i++, base_c += h_stride) {
+//        fwrite(base_c, 1, width, fp);
+        memcpy(yuvImg.data+idx, base_c, width);
+        idx += width;
+    }
+    
+    cv::Mat rgbImg;
+    cv::cvtColor(yuvImg, rgbImg, CV_YUV420sp2BGRA);
+    cv::imwrite("/home/firefly/zlatan/newmpp.jpg", rgbImg);
+
+
+
+}
+
 void dump_mpp_frame_to_file(MppFrame frame, FILE *fp)
 {
     RK_U32 width    = 0;
